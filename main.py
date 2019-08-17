257f90a7c0ce0cfb473884a79f8e40840bc03462
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2019-08-17 12:23:04 -0500

    first solution

--- main.py
+++ main.py
@@ -4,46 +4,132 @@ from os.path import isfile, join
 import csv
 import numpy as np
 import math
+import operator
 
-directory = "test/"
+directory = "resources/"
 
 def main():
     onlyfiles = [file for file in listdir(directory) if isfile(join(directory, file))]
 
     for file in onlyfiles: 
-        (K, customerPositions, customerPositionsDemand) = readFile(directory + file)
+        (K, customerPositions, demands) = read_file(directory + file)
 
-        # Enter NumPy
-        numberOfCustomers = len(customerPositions)
+        # Initialization
+        demand_nodes = customerPositions[1:]
+        numberOfCustomers = len(customerPositions) - 1
         vehicleCapacities = K['Q']
         depot = customerPositions[0]
         pointsLen = len(customerPositions)
+        routes = dict()
+        idx = -1 # route number
+        vehicleCap = 0
+
+        # None of the customers have been visited
+        visited = dict()
+        for c in demand_nodes: visited[c] = False
+        
+        # build (x, y) -> demand
+        customerPositionsDemand = dict()
+        for (coord, demand) in zip(demand_nodes, demands[1:]):
+            customerPositionsDemand[coord] = demand
 
 
         # Improved savings method. Refer to: 
         # http://ieeexplore.ieee.org/document/7784340/?reload=true
         
-        # Step 1 & 2-- Calculate savings using distances  
-        demand_nodes = customerPositions[1:]
-        savings = np.sort(calculate_savings(depot, demand_nodes).ravel())
-        savings = savings[::-1] # Reverse
+        # Step 1 & 2-- Calculate savings using distances
+        savings = calculate_savings(depot, demand_nodes)
+        savings = sorted(savings.items(),key=operator.itemgetter(1),reverse=True)
+        cost_pairs = list()
 
-        # Step 3 -- Choose two customers for the initial route
+        for i in range(len(savings)):
+            cost_pairs.append(savings[i][0])
+
+        while(False in visited.values()):
+            # Step 3 -- Choose two customers for the initial route
+            for c in cost_pairs:
+                if (visited[c[0]] == False and visited[c[1]] == False):
+                    visited[c[0]], visited[c[1]] = (True, True)
+                    idx += 1
+                    routes[idx] = ([c[0],c[1]])
+                    vehicleCap = 200 #vehicleCapacities[idx]
+                    
+                    break
+            
+            # Step 4 -- Finding a feasible cust that is either at the start or end of previous route
+            for c in cost_pairs:
+                res = inPrevious(c[0], routes[idx])
+
+                if (res == 0 and capacityValid(routes[idx], c[0], customerPositionsDemand, vehicleCap) and visited[c[0]] == False):
+                    visited[c[1]] = True
+                    routes[idx].append(c[1])
+                elif (res == 1 and capacityValid(routes[idx], c[0], customerPositionsDemand, vehicleCap) and visited[c[0]] == False):
+                    visited[c[1]] = True
+                    routes[idx].insert(0, c[1])
+                else: 
+                    res = inPrevious(c[1], routes[idx])
+                    if (res == 0 and capacityValid(routes[idx], c[0], customerPositionsDemand, vehicleCap) and visited[c[0]] == False):
+                        visited[c[0]] = True
+                        routes[idx].append(c[0])
+                    elif (res == 1 and capacityValid(routes[idx], c[0], customerPositionsDemand, vehicleCap) and visited[c[0]] == False):
+                        visited[c[0]] = True
+                        routes[idx].insert(0, c[0])
+
+                # Step 5 -- Repeat 4 till no customer can be added to the route (for)
+
+            # Step 6 -- Repeat 3, 4, 5 till all customers are added to some route (while)
+        checkSolution(routes, visited, customerPositionsDemand, vehicleCapacities)
 
 
-        # Step 4 -- Magic step
+def checkSolution(routes, visited, customerPositionsDemand, vehicleCapacities):
+    totalCapacity = 0
+    for route in routes:
+        totalRoute = 0
+        for node in routes[route]:
+            totalRoute += customerPositionsDemand[node]
+        totalCapacity += totalRoute
+        
+    if not False in visited.values(): print("All nodes visited")
+    else: print("Not all nodes visited")
+    print("Number of kids picked up ", totalCapacity, " out of ", sum(customerPositionsDemand.values()))
+    print("Number of routes", len(routes), " out of ", len(vehicleCapacities))
 
+def capacityValid(existing, new, customerPositionsDemand, vehicleCap):
+    totalCap = customerPositionsDemand[new]
+    for c in existing:
+        totalCap += customerPositionsDemand[c]
 
-        # Step 5 -- Repeat 4 till no customer can be added to the route 
+    return totalCap <= vehicleCap
 
+'''
+    Returns wether or not the 'new' node is in the 'existing' route
+    1 if it is at the begininig of the route
+    0 if it is at the end 
+    -1 otherwise
+'''
+def inPrevious(new, existing):
+    start = existing[0]
+    end = existing[len(existing)-1]
+    if new == start:
+        return 1
+    elif new == end:
+        return 0
+    else:
+        return -1
 
-        # Step 6 -- Repeat 3, 4, 5 till all customers are added to some route
-
+'''
+    Get and remove max item from an array 
+'''
+def get_max_and_remove(array):
+    n = np.amax(array)
+    indexes = np.where(array == np.amax(array))
+    array[indexes[0], indexes[1]] = 0
+    return (n, indexes, array)
 
 '''
     Reads the input file and returns a tuple with (R, coords(x, y), q)
 '''
-def readFile(path):
+def read_file(path):
     with open(path) as file:
             line = file.readline().split('\t')
             n = int(line[0]) # Number of demand nodes
@@ -91,11 +177,11 @@ def calculate_distances(points):
 '''
 def calculate_savings(depot, points):
     n = len(points)
-    s = np.zeros((n, n))
+    s = dict()
     for i in range(0, n):
-        for j in range(0, n):
+        for j in range(0, i):
             if (i != j):
-                s[i, j] = s[j, i] = saving(depot, points[i], points[j])
+                s[(points[i], points[j])] = saving(depot, points[i], points[j])
     
     return s
 
